3c3a488d9fc9001973b93fa42a26ed3362630257
Maksym Lizohub <maksym.lizohub@gmail.com>
2022-05-09 15:01:56 +0300

    mod 6

--- js/modal.js
+++ js/modal.js
@@ -26,3 +26,17 @@
       e.currentTarget.reset();
     });
 })();
+
+(() => {
+  document
+    .querySelector('.js-submit-form')
+    .addEventListener('submit', e => {
+      e.preventDefault();
+
+      new FormData(e.currentTarget).forEach((value, name) =>
+        console.log(`${name}: ${value}`),
+      );
+
+      e.currentTarget.reset();
+    });
+})();
